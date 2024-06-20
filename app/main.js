f9faa792d4da4ede3854c4ad4bfa51e95f07742c
unknown <walterg_naco@yahoo.com.ar>
2024-06-20 13:54:26 -0300

    inicio de proyecto

---
+++ app/main.js
@@ -0,0 +1,85 @@
+const $divCards = document.querySelector('.grid');
+const $btns = document.querySelectorAll('.btn');
+const $scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+const url = 'https://hp-api.onrender.com/api/characters';
+
+fetch(url)
+    .then(response => response.json())
+    .then(database => showData(database));
+    
+// Tomamos los datos de la api y los mostramos en pantalla
+function showData(data) {
+    $divCards.innerHTML = '';
+    data.forEach(character => {
+        let person = character.name.replace(' ', '+');
+        let actor = 'Actor';
+
+        if (character.gender === 'female') {
+            actor = 'Actris';
+        } 
+        let estado = person.alive;
+        if (estado) {
+          estado = "vivo";
+        } else {
+          estado = "fallecido";
+        }
+        if(!character.actor) {
+            actor = 'Googlealo..';
+        }
+        if(character.house) {
+            $divCards.innerHTML += `
+            <a href="https://www.google.com/search?q=${person}&oq=harr&gs_lcrp=EgZjaHJvbWUqDwgAEEUYOxiDARixAxiABDIPCAAQRRg7GIMBGLEDGIAEMgYIARBFGDkyCggCEAAYsQMYgAQyDQgDEAAYgwEYsQMYgAQyCggEEAAYsQMYgAQyBggFEEUYPDIGCAYQRRg9MgYIBxBFGD0yBggIEEUYQdIBCDEzNDFqMGo0qAIAsAIB&sourceid=chrome&ie=UTF-8" 
+            target="_blank" class="card ${character.house.toLowerCase()}">
+                <div>
+                    <h4>${character.name}</h4>
+                    <img src="${character.image ? character.image : '../assets/middle.png'}" 
+                        class="${character.image ? 'imgCards' : 'imgDefaults'}" 
+                        alt="imagen de ${character.name}"
+                    >
+                    <p class="character-house"><span>${character.house}</span></p>
+                    <p class="character-house"><span>${actor}:</span> ${character.actor}</p>
+                    <p>Estado: ${estado}</p>
+                </div>
+            </a>
+            `;
+        }
+    }); 
+};
+
+// Iteramos los botones para filtrar segun la casa Hogwarts
+$btns.forEach($btn => {
+    $btn.addEventListener('click', (e) => {
+        const btnHouse = e.currentTarget.id.toLowerCase();
+        $divCards.innerHTML = '';
+        fetch(url)
+        .then(response => response.json())
+        .then(database => {
+            if (btnHouse === 'all') {
+                showData(database);
+            } else {
+               const filteredData = database.filter(data => data.house.toLowerCase() === btnHouse);
+            showData(filteredData);
+            }       
+            
+        });
+    });
+});
+
+// Mostrar el botón al desplazarse y desplazarse hacia arriba
+$scrollToTopBtn.addEventListener('click', scrollToTop);
+
+function scrollFunction() {
+  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    scrollToTopBtn.classList.add("show");
+  } else {
+    scrollToTopBtn.classList.remove("show");
+  }
+}
+
+onscroll = () => scrollFunction();
+
+function scrollToTop() {
+  scrollTo({top: 0, behavior: 'smooth'});
+}
---
+++ css/README.md
@@ -0,0 +1,4 @@
+TP N°2 - Javascript, utilizando API de Harry Potter
+
+Detalle del proyecto:
+Utilizamos las herramientas de HTML, CSS y JS, relacionando dos paginas y haciendolas un sitio multiuso, con los datos de la API de Harry Potter https://hp-api.onrender.com/api/characters.
